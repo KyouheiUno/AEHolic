fb61f2fcfc077c3c6c39baa04c33097fd9c50adf
kyohei uno <waizeru00@gmail.com>
2023-02-10 07:43:57 +0900

    first commit

---
+++ AEHolic/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  AEHolic
+//
+//  Created by 宇野恭平 on R 5/02/10.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("HomeViewControllerの画面です")
+    }
+}
