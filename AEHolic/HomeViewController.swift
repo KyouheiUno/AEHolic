8e0f8b5b2004d0ad3a342f670359a606fc9e5fab
kyohei uno <waizeru00@gmail.com>
2023-02-10 08:19:48 +0900

    navigationBarにタイトルを設定

--- AEHolic/HomeViewController.swift
+++ AEHolic/HomeViewController.swift
@@ -9,8 +9,12 @@ import Foundation
 import UIKit
 
 class HomeViewController: UIViewController {
-    
     override func viewDidLoad() {
-        
+        setupNavigationBarTitle()
+    }
+    //ナビゲーションタイトルの設定
+    func setupNavigationBarTitle() {
+        title  = "AEHolic"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
