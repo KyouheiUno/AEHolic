3f979de5a96eaacba74556206d1614e0b9a3154a
kyohei uno <waizeru00@gmail.com>
2023-02-10 11:28:32 +0900

    import Foundationの削除とタブ名の表記表示を追加

--- AEHolic/HistoryViewController.swift
+++ AEHolic/HistoryViewController.swift
@@ -4,19 +4,17 @@
 //
 //  Created by 宇野恭平 on R 5/02/10.
 //
-
-import Foundation
 import UIKit
 
 class HistoryViewController: UIViewController {
     override func viewDidLoad() {
+        super.viewDidLoad()
         setupNavigationBarTitle()
         debugCheck() //最後は消しましょう
     }
     //ナビゲーションタイトルの設定
     private func setupNavigationBarTitle() {
         title  = "累積記録"
-        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     //デバック用として最後は消しましょう
--- AEHolic/IndexViewController.swift
+++ AEHolic/IndexViewController.swift
@@ -4,12 +4,11 @@
 //
 //  Created by 宇野恭平 on R 5/02/10.
 //
-
-import Foundation
 import UIKit
 
 class IndexViewController: UIViewController {
     override func viewDidLoad() {
+        super.viewDidLoad()
         setupNavigationBarTitle()
         debugCheck()
     }
@@ -17,7 +16,6 @@ class IndexViewController: UIViewController {
     //ナビゲーションタイトルの設定
     private func setupNavigationBarTitle() {
         title  = "Eバンド一覧"
-        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     //デバック用として最後は消しましょう
