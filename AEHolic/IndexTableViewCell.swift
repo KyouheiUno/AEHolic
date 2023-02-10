c627df270e8eb950a8cd0f7e692da7ed9cae1061
kyohei uno <waizeru00@gmail.com>
2023-02-11 06:23:01 +0900

    テンプレート変更により一部削除

--- AEHolic/IndexTableViewCell.swift
+++ AEHolic/IndexTableViewCell.swift
@@ -10,18 +10,12 @@ import UIKit
 class IndexTableViewCell: UITableViewCell {
 
     @IBOutlet weak var indexContentLabel: UILabel!
-    @IBOutlet weak var indexStartDateLabel: UILabel!
-    @IBOutlet weak var indexFinishDateLabel: UILabel!
-    @IBOutlet weak var indexTotalRecordLabel: UILabel!
-    @IBOutlet weak var indexSuccessCountLabel: UILabel!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         indexContentLabel.text = "ランニング"
-        indexStartDateLabel.text = "yyyy/mm/dd"
-        indexFinishDateLabel.text = "継続中"
-        indexTotalRecordLabel.text = "累積記録"
-        indexSuccessCountLabel.text = "達成回数"
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
