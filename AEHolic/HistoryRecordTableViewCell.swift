ff5a3f84ea06976c9a0fe956dcc578eb369f519f
kyohei uno <waizeru00@gmail.com>
2023-02-11 06:44:53 +0900

    累積記録画面のカスタムセルを作成

---
+++ AEHolic/HistoryRecordTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  HistoryRecordTableViewCell.swift
+//  AEHolic
+//
+//  Created by 宇野恭平 on R 5/02/11.
+//
+
+import UIKit
+
+class HistoryRecordTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var historyRecordContentLabel: UILabel!
+    @IBOutlet weak var historyRecordStartDateLabel: UILabel!
+    @IBOutlet weak var historyRecordFinishDateLabel: UILabel!
+    @IBOutlet weak var historyRecordTotalValueLabel: UILabel!
+    @IBOutlet weak var historyRecordSuccessCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        historyRecordContentLabel.text = "ランニング"
+        historyRecordStartDateLabel.text = "yyyy/mm/dd"
+        historyRecordFinishDateLabel.text = "継続中"
+        historyRecordTotalValueLabel.text = "累積記録"
+        historyRecordSuccessCountLabel.text = "達成回数"
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- AEHolic/HistoryViewController.swift
+++ AEHolic/HistoryViewController.swift
@@ -6,13 +6,26 @@
 //
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var historyRecordTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBarTitle()
+        historyRecordTableView.register(UINib(nibName: "HistoryRecordTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
     }
     //ナビゲーションタイトルの設定
     private func setupNavigationBarTitle() {
         title  = "累積記録"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! HistoryRecordTableViewCell
+        return cell
     }
 }
