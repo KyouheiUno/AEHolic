788c67ba57921178dd8381f0c877a5e2fc14daa0
kyohei uno <waizeru00@gmail.com>
2023-02-11 11:45:54 +0900

    ステータス画像のアップロードとコードによる表示の実装

--- AEHolic/cell/HomeTableViewCell.swift
+++ AEHolic/cell/HomeTableViewCell.swift
@@ -15,8 +15,18 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var homeActionCountLabel: UILabel!
     @IBOutlet weak var homeHistoryRecordLabel: UILabel!
     
+    //image
+    let userDefaultIcon = UIImage(named: "userDefaultIcon")!
+    let successIcon = UIImage(named: "successIcon")!
+    let failureIcon = UIImage(named: "failureIcon")!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        homeUserIconImage.image = userDefaultIcon
+        //この箇所を出し分ける
+        homeBandStatusIconImage.image = successIcon
+        //homeBandStatusIconImage.image = failureIcon
+        //この箇所を出し分ける
         homeContentLabel.text = "表示のテスト表示のテスト表示のテスト表示のテスト表示のテスト表示のテスト表示のテスト表示のテスト"
         homeActionCountLabel.text = "実行数" + "　" + "〇〇" + "回"
         homeHistoryRecordLabel.text = "累積記録　〇〇〇〇→〇〇〇〇 "
@@ -27,5 +37,4 @@ class HomeTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
 }
